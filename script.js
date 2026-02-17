3b63cf3201af38a673fc369ef1466ac49225ccc4
Md Muzamil Ahmad <120739641+muzamilmujju@users.noreply.github.com>
2026-02-17 13:00:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showTab(tabId) {
+  const tabs = document.querySelectorAll('.tab');
+  tabs.forEach(tab => tab.classList.remove('active'));
+
+  document.getElementById(tabId).classList.add('active');
+}
